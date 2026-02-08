1a55eed911bb3b706d0fd96d018b4fc37a1bd36a
Sebastián Briñez <johansebastianbrinezparra@gmail.coml>
2026-02-08 13:02:34 -0500

    Fix: base config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración para GitHub Pages
 export default defineConfig({
-  base: '/MOTOX-STORE/', // Muy importante para que los assets se carguen correctamente
   plugins: [react()],
 })
